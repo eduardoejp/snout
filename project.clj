721468a38df8441c63062a05c7636e1986040b70
Eduardo Emilio Julián Pereyra <eduardoejp@gmail.com>
2012-05-15 10:17:29 -0400

    v0.1.0

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject snout "0.1.0"
+  :description "Client-side route-matching lib for ClojureScript.",
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  )
---
+++ src/snout/macros.clj
@@ -0,0 +1,37 @@
+
+(ns snout.macros)
+
+; <Constants>
+(def ^:private +splat+ #"\*")
+(def ^:private +literal+ #"/\w*")
+(def ^:private +keyword+  #"/:[\w\-]+")
+(def ^:private +params+ #"\?.*")
+
+; <Utils>
+(defn- lex1 [route]
+  (if-let [x (and (.startsWith route "/:") (re-find +keyword+ route))]
+    [(keyword (.substring x 2)) (.substring route (count x))]
+    (if-let [x (re-find +splat+ route)]
+      ["*" nil]
+      (if-let [x (re-find +literal+ route)]
+        [x (.substring route (count x))]
+        (if-let [x (re-find +params+ route)]
+          ["?" (.substring x 1)])))))
+
+(defn- make-matcher [route]
+  (loop [[type route] (lex1 route)
+         matcher []]
+    (if (nil? type)
+      matcher
+      (recur (lex1 route)
+             (conj matcher type)))))
+
+; <API>
+(defmacro defroute
+  "Given a route (the same style as a Clout route), an args-vector and a body,
+defines a function that will be called when the fragment identifier of the current URL changes and is matched by the route.
+The route will be passed :route :params as well as parameters like those of a GET request.
+e.g. (defroute \"/my/route/:arg1\" [arg1 arg2 arg2]
+       (js/alert (pr-str arg1 arg2 arg2)))"
+  [route args & body]
+  `(swap! snout.core/routes conj [~(make-matcher route) (fn [{:keys ~args}] ~@body)]))
